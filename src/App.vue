f5f6be65456db6071253b2d51eab6d1eb81c510f
yejinnnnnhi <119246215+yejinnnnnhi@users.noreply.github.com>
2023-01-17 14:59:28 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,38 @@
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="150" height="150" />
+    <div>
+      <h1>Welcome</h1>
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+      </ul>
+
+      <form>
+        <input type="search" name="q" placeholder="Search a movie" />
+        <input type="submit" value="Click" />
+      </form>
+    </nav>
+  </header>
+
+  <main>
+    <label for="movie recommend">Movie of This Month : </label>
+    <select id="title">
+      <option value="">--Please choose a title--</option>
+      <option value="Avatar">Avatar2</option>
+      <option value="RRR">RRR</option>
+      <option value="기생충">기생충</option>
+    </select>
+  </main>
 </template>
 
-<style>
-
+<style scoped>
+header {
+  line-height: 2;
+  max-height: 100vh;
+}
 </style>
